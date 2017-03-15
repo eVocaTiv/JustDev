ee2fc1a8fe7958c8fa72d611e29840f697557dc1
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-03-15 21:03:55 +0530

    'PRIORITY QUEUE INBUILT JAVA

--- DSA TUT 1 - TREES/Main.java
+++ DSA TUT 1 - TREES/Main.java
@@ -1,128 +1,42 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
-
-
 public class Main {
-	static Node root;
-	static ArrayList<Integer> Inorderlist = new ArrayList<Integer>();
-	static Scanner s = new Scanner(System.in);
-	public static void main(String[] args) {
-		root= new Node(8);
-		root.left = new Node(3);
-		root.right = new Node(10);
-		root.left.left = new Node(1);
-		root.left.right= new Node(6);
-		root.left.right.right = new Node(7);
-		root.left.right.left = new Node(4);
-		root.right.right= new Node(14);
-		root .right . right .left = new Node(13);
-
-		System.out.println("Please enter your desired number");
-		
-		System.out.println(FindClosestAns(root , s.nextInt()));
-
-	}
-
-	public static int FindClosestAns(Node focus , int val){
-		return val + FindClosest(focus , val , 0 , Integer.MAX_VALUE);
-	}
-
-
-	public static int FindClosest(Node focus , int val , int diff , int mindiff){
-		if(focus==null)
-			return mindiff;
-		diff = focus.key - val;
-
-		if(diff==0)
+	static PriorityQueue<Mix> list = new PriorityQueue<>(3 , new Comparator<Mix>(){
+		public int compare(Mix m1 , Mix m2){
+			if(m1.b > m2.b)
+				return 1;
+			if(m1.b < m2.b)
+				return -1;
 			return 0;
-
-		if(Math.abs(diff) < Math.abs(mindiff))
-			mindiff = diff;
-
-		if(Math.abs(diff) == Math.abs(mindiff)){
-			if(diff < 0)
-				mindiff = diff;
 		}
-		if(focus.key>val){
-			//System.out.println("focus key is " + focus.key);
-			if(focus.left!=null)
-				return FindClosest(focus.left , val , diff, mindiff);
-			return mindiff;
+	});
+	public static class Mix{
+		private int a;
+		private int b;
+		
+		Mix(int a , int b){
+			this.a=a;
+			this.b=b;
 		}
-		else
-			if(focus.right!=null)
-				return FindClosest(focus.right , val, diff, mindiff);
-			return mindiff;
-
 	}
-
-
-	public static void Inorder(Node focus){
-		if(focus.left!=null)
-			Inorder(focus.left);
-		Inorderlist.add(focus.key);
-		if(focus.right!=null)
-			Inorder(focus.right);
-
-	}
-
-
-	public static void SumCheck(int sum){
-		Inorder(root);
-		Collections.sort(Inorderlist);
-		int l=0;
-		int tsum=0;
-		int r=Inorderlist.size()-1;
-		while(l<r){
-			tsum=Inorderlist.get(l) + Inorderlist.get(r);
-			if(sum==tsum){
-				System.out.println("True");
-				return;
-			}
-			else
-				if(sum>tsum){
-					//System.out.println("High and Sum is" + tsum);
-					l++;
-				}
-				else
-					if(sum<tsum){
-						System.out.println("Low");	
-						r--;
-					}
-
+	
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		
+		for(int i=0 ; i<n ; i++){
+			list.add(new Mix(s.nextInt() , s.nextInt()));
 		}
-
-		System.out.println("False");
-	}
-
-	public static void PLV(){
-		Queue<Node> q = new LinkedList<Node>();
-		q.add(root);
-
-		root.level=0;
-		int currentlevel=0;
-		while(!q.isEmpty()){
-			Node top = q.remove();
-			if(top.level==currentlevel){
-				System.out.println(top.key);
-				currentlevel++;
-			}
-			if(top.left!=null){
-				q.add(top.left);
-				top.left.level=top.level+1;
-			}
-			if(top.right!=null){
-				q.add(top.right);
-				top.right.level=top.level+1;
-			}
-
+	
+		while(!list.isEmpty()){
+			Mix m = list.poll();
+			System.out.println(m.a + " " + m.b);
 		}
-
-
-
+		
+		s.close();
 	}
 }
