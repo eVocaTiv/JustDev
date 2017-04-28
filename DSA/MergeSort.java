6f63777f939149ba748534c7162e883306e18aa0
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-04-28 03:58:33 -0800

    Create MergeSort.java

---
+++ DSA/MergeSort.java
@@ -0,0 +1,58 @@
+/*
+ * Java Program to Implement Merge Sort
+ */
+ 
+import java.util.Scanner;
+ 
+/* Class MergeSort */
+public class Main 
+{
+    /* Merge Sort function */
+    public static void sort(int[] a, int low, int high) 
+    {
+        int N = high - low;         
+        if (N <= 1) 
+            return; 
+        int mid = low + N/2; 
+        // recursively sort 
+        sort(a, low, mid); 
+        sort(a, mid, high); 
+        // merge two sorted subarrays
+        int[] temp = new int[N];
+        int i = low, j = mid;
+        for (int k = 0; k < N; k++) 
+        {
+            if (i == mid)  
+                temp[k] = a[j++];
+            else if (j == high) 
+                temp[k] = a[i++];
+            else if (a[j]<a[i]) 
+                temp[k] = a[j++];
+            else 
+                temp[k] = a[i++];
+        }    
+        for (int k = 0; k < N; k++) 
+            a[low + k] = temp[k];         
+    }
+    /* Main method */
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner( System.in );        
+      //  System.out.println("Merge Sort Test\n");
+        int n, i;
+        /* Accept number of elements */
+   
+        n = scan.nextInt();
+        /* Create array of n elements */
+        int arr[] = new int[ n ];
+        /* Accept elements */
+        for (i = 0; i < n; i++)
+            arr[i] = scan.nextInt();
+        /* Call method sort */
+        sort(arr, 0, n);
+        /* Print sorted Array */
+        for (i = 0; i < n; i++)
+            System.out.print(arr[i]+" ");            
+        System.out.println();            
+    }    
+}
