8b154337e1f7b0c0e1eb3a87d10cc74d81e99858
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-05-25 17:39:24 +0530

    Create String Practice.java

---
+++ PS-1 -Sheekhsha- and related Code/String Practice.java
@@ -0,0 +1,44 @@
+
+public class Main {
+	public static void main(String[] args){
+
+		String s = "Helloooo  ";
+		String[] Arr = s.split(" ");
+
+		//	for(String str : Arr)
+		//	System.out.println(str);
+
+		String x;
+
+		//	s=s.substring(0 ,5);
+		//System.out.println(s);
+		//	x=s;
+		//System.out.println(x);
+		//System.out.println(x.substring(0, x.length()-1));
+
+
+		//System.out.println(x.concat("Hi"));
+		//	s.concat("Heyy");
+
+		//System.out.println(s=s.concat("Hey"));
+		//System.out.println(s);
+
+		//System.out.println(x=(s=s.concat("Lol").substring(0, 10)));
+
+
+		//System.out.println(s.indexOf(' '));
+		//String k = new String(s.substring(1 , 2).concat("Modified"));
+		//System.out.println(k);
+
+		System.out.println(s);
+		System.out.println(s.length());
+
+		s=s.trim();
+
+
+		System.out.println(s);
+		System.out.println(s.length());
+
+		//System.out.println(s.compareToIgnoreCase("kunal"));
+	}
+}
