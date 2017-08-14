2393a758f85fd4758c6dd3075676c06d52f98f5f
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-08-15 01:55:10 +0530

    Create Q2.java

---
+++ EnterProgramming./AugustChallenge/Q2.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	static int t , D , diff, n , res;
+	static float x=0;
+	static int xint;
+	private static Scanner s;
+	private static int[] list;
+
+
+	public static void main(String[] args) {
+		boolean np = true;
+		s = new Scanner(System.in);
+		t=s.nextInt();
+
+		while(t-- > 0) {
+
+			res = 0;
+			n = s.nextInt();
+			list = new int[n];
+			D = s.nextInt();
+
+
+
+
+			for(int i=0 ; i<n ; i++)
+				list[i]=s.nextInt();
+
+			x=0;
+			for(int el : list) {
+				x+=el;
+			}
+			x/=n;
+
+
+			//System.out.println("hi");
+			//System.out.println(x);
+			//System.out.println(x);
+			if(x==(int)x) {
+				np = false;
+				xint = (int)x;
+				//System.out.println("X val is " + xint);
+				GetAns();
+			}
+			if(x==(int)x)
+				for(int el : list) {
+					if(el!=xint)
+						np = true;
+				}
+
+			System.out.println(np == true? -1 : res);
+			np = true;
+
+		}
+	}
+
+	public static void GetAns() {
+		//System.out.println("in getans");
+
+		for(int i=0 ; i<n ; i++) {
+			if(list[i]==xint)
+				;
+			else 
+				if(list[i]<xint) {
+					if(i+D<n) {
+						diff = xint-list[i];
+						list[i] = xint;
+						res += diff;
+						list[i+D]-=diff;
+					}
+				}
+				else {
+					if(i+D<n) {
+						diff = list[i]-xint;
+						list[i] = xint;
+						res += diff;
+						list[i+D]+=diff;
+						
+					}
+				}
+		}
+
+
+	}
+
+
+
+}
