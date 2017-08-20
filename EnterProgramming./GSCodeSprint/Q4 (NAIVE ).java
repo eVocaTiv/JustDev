2efabc0ba812b0fd86488a0996cbc49d85e22964
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-08-21 01:49:03 +0530

    Update Q4 (NAIVE ).java

--- EnterProgramming./GSCodeSprint/Q4 (NAIVE ).java
+++ EnterProgramming./GSCodeSprint/Q4 (NAIVE ).java
@@ -4,68 +4,74 @@ import java.util.Scanner;
 
 //NAIVE SOLUTION
 
-public class Main {
+public class Solution {
 	static int[] p , t;
-	
+
 	public static int GetAns(int type , int v , int n) {
-	
+
 		if(type==1) {
+			//O(N)...
 			for(int i=0 ; i<n ; i++) {
 				if(p[i]>v) {
 					return t[i];
 				}
 			}
-			
+
 			//TYPE 1 QUERY.
 		}
 		else {
+			//O(logn) + O(n) = O(n)....
 			int rawindex = Arrays.binarySearch(t, v);
 			//rawindex = -(indexpoint ) - 1 ;
-		//	System.out.println("RawIndex is" + rawindex);
-			
+			//	System.out.println("RawIndex is" + rawindex);
+
 			int index = rawindex;
 			if(index < 0) {
 				index ++;
 				index = - index;
 			}
-				//System.out.println("Index is " + index);
+			//System.out.println("Index is " + index);
 			int maxindex = index;
-			
+
 			for(int j = index ; j<n ; j++) {
 				if(p[j]>p[maxindex]) {
 					maxindex = j;
 				}
-				
-				
+
+
 			}
 			return p[maxindex];
-			
+
 		}
-		
+
 		return -1;
 	}
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int q = in.nextInt();
-        t = new int[n];
-        for(int t_i = 0; t_i < n; t_i++){
-            t[t_i] = in.nextInt();
-        }
-        p = new int[n];
-        for(int p_i = 0; p_i < n; p_i++){
-            p[p_i] = in.nextInt();
-        }
-        for(int a0 = 0; a0 < q; a0++){
-            int _type = in.nextInt();
-            int v = in.nextInt();
-            
-           System.out.println( GetAns(_type , v , n) );
-      
-        }
-        
-        
-        in.close();
-    }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int q = in.nextInt();
+		t = new int[n];
+		for(int t_i = 0; t_i < n; t_i++){
+			t[t_i] = in.nextInt();
+		}
+		p = new int[n];
+		for(int p_i = 0; p_i < n; p_i++){
+			p[p_i] = in.nextInt();
+		}
+		
+		
+		// O(Q*LOGN) .. EXCEEDS TIME LIMIT!
+		for(int a0 = 0; a0 < q; a0++){
+			int _type = in.nextInt();
+			int v = in.nextInt();
+
+			
+			System.out.println( GetAns(_type , v , n) );
+
+		}
+
+
+		in.close();
+	}
 }
