dbdc965714b222a6d37bd31d51d5efb164a4e617
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-09-01 18:26:45 +0530

    Create Q1.java

---
+++ EnterProgramming./SeptemberChallenger/Q1.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	private static Scanner sc;
+	private static int t , n;
+	private static long[]  prefix , suffix;
+	private static int[] a;
+	public static void init(){
+
+
+		n = sc.nextInt();
+
+		a = new int[n];
+		prefix = new long[n];
+		suffix = new long[n];
+
+		for(int i=0 ; i<n ; i++){
+			a[i] = sc.nextInt();
+		}
+
+	}
+
+	public static void GetAns(){
+		prefix[0]=a[0];
+		suffix[n-1] = a[n-1];
+		//compute prefix.
+
+		for(int i=1 ; i<n ; i++){
+			prefix[i]= prefix[i-1] + a[i];
+		}
+
+		//compute suffix.
+		for(int i=n-2 ; i>=0 ; i--){
+			suffix[i]= suffix[i+1] + a[i];
+		}
+
+		long sum = Integer.MAX_VALUE ;
+		int index = 0;
+		for(int i=0 ; i<n ;i++){
+			if(sum>prefix[i] + suffix[i]){
+				sum = Math.min(sum , prefix[i] + suffix[i]);
+				index = i;
+			}
+		}
+
+		
+		index++;
+		System.out.println(index);
+
+
+
+	}
+
+	public static void main(String[] args){
+		sc = new Scanner(System.in);
+		t = sc.nextInt();
+
+
+
+		while(t-- > 0){
+			init();
+			GetAns();
+		}
+	}
+
+
+}
