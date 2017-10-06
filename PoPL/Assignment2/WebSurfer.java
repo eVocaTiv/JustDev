ed7a41795d810c5d914c4b1298b8fb3f0d26e923
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2017-10-06 23:51:49 +0530

    Create WebSurfer.java

---
+++ PoPL/Assignment2/WebSurfer.java
@@ -0,0 +1,26 @@
+
+/*
+ * AUTHOR 2015A7PS0011G
+ * KUNAL DEWAN.
+ * 
+ */
+
+
+//FULLY WORKING PROGRAM!!
+//GOOD ASSIGNMENT.
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WebSurfer{
+	
+	public static void main(String[] args) {
+		Tab t1 = new Tab(3 , "google.com");
+		//Tab t2 = new Tab(3 , "yahoo.com");
+		Tab t2 = new Tab(5 , "yahoo.com");
+
+		
+	}
+
+}
