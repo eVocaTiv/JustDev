46b184841ed4c659384f5039b805d0b1b9434ebf
Kunal Dewan <f2015011@goa.bits-pilani.ac.in>
2018-04-25 21:02:56 +0530

    Update prac2.tcl

--- Computer Networks/NS2/PracticeForExam/prac2.tcl
+++ Computer Networks/NS2/PracticeForExam/prac2.tcl
@@ -23,7 +23,7 @@ proc finish {} {
 
 
 
-for {set i 0} {$i < 7 } {incr i} {
+for {set i 0} {$i < 4 } {incr i} {
 
 set n$i [$ns node]
 
@@ -31,17 +31,61 @@ set n$i [$ns node]
 
 #CREATE LINKS
 
-$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n0 $n1 2Mb 10ms SFQ
+$ns duplex-link $n2 $n1 2Mb 10ms SFQ
+$ns duplex-link $n1 $n3 2Mb 10ms SFQ
+$ns duplex-link $n3 $n2 2Mb 10ms SFQ
 
 
 
 #FIX ORIENTATION
-$ns duplex-link-op $n0 $n1 orient right-up
+$ns duplex-link-op $n1 $n0 orient left
+$ns duplex-link-op $n1 $n2 orient right
+$ns duplex-link-op $n1 $n3 orient down
+$ns duplex-link-op $n3 $n2 orient up-right
+
+#SET QUEUE LIMIT
+
+$ns queue-limit $n1 $n0 1000
 
 
+#CREATE TCP AGENTS
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 set packetSize_ 500
+$cbr0 set interval_ 0.003
+$cbr0 attach-agent $tcp0
+
+set tcp1 [new Agent/TCP]
+$ns attach-agent $n0 $tcp1
+
+set cbr1 [new Application/Traffic/CBR]
+$cbr1 set packetSize_ 500
+$cbr1 set interval_ 0.003
+$cbr1 attach-agent $tcp1
+
+#CREATE SINK AGENTS.
+set null2 [new Agent/TCPSink]
+$ns attach-agent $n2 $null2
+
+set null3 [new Agent/TCPSink]
+$ns attach-agent $n3 $null3
+
+#CONNECT PATHS.
+
+$ns connect $tcp0 $null2
+$ns connect $tcp1 $null3
 
 
+$ns at 0.5 "$cbr0 start"
+$ns at 0.5 "$cbr1 start"
+$ns rtmodel-at 1.5 down $n1 $n2
+$ns at 4.5 "$cbr0 stop"
+$ns at 4.5 "$cbr1 stop"
 
+$ns rtproto DV
 
 $ns at 5.0 "finish"
 $ns run
